056937b780c12077459681e71b388152fe17b964
sasham43 <sashasemail@gmail.com>
2016-09-15 15:23:38 -0500

    add extra images & image upload

--- client/client.js
+++ client/client.js
@@ -5,7 +5,24 @@ angular.module('brouchard', []);
 angular.module('brouchard').controller('MainController', ['$http', function($http){
   var mc = this;
 
-  var neural = new synaptic.Architect.LSTM(2,15,3);
+  // file upload
+  mc.previewFile = function(){
+    var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  };
+
+  var neural = new synaptic.Architect.Perceptron(2,15,3);
   // var worker = null; // ?
   var index = 0; // ?
   var image_data = null;
