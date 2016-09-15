fbd443380184bac167c14345def12c0e43e27863
sasham43 <sashasemail@gmail.com>
2016-09-15 14:55:55 -0500

    switch neural network to client side

--- client/client.js
+++ client/client.js
@@ -1,5 +1,71 @@
 angular.module('brouchard', []);
 
+// var socket = io();
+
 angular.module('brouchard').controller('MainController', ['$http', function($http){
+  var mc = this;
+
+  var neural = new synaptic.Architect.LSTM(2,15,3);
+  // var worker = null; // ?
+  var index = 0; // ?
+  var image_data = null;
+  var canvas = null;
+  var context = null;
+  // var size = 125 * 125;
+  var iteration = 0; // ?
+  // var to = null; // ?
+  var px = null;
+
+  var getData = function(imageObj){
+    canvas = canvas || document.getElementById('output');
+    context = context || canvas.getContext('2d');
+
+    context.drawImage(imageObj, 0, 0);
+    var imageData = context.getImageData(0, 0, 125, 125);
+    return imageData.data;
+  };
+
+  var train = mc.train = function(){
+    // lstm = new synaptic.Architect.LSTM(2, 15, 3);
+    image_data = getData(document.getElementById('input'));
+    console.log('training...');
+    paint();
+  };
+
+  var iterate = function(){
+    for(var x = 0; x < 125; x++){
+      for(var y = 0; y < 125; y++){
+        var dynamicRate = .01/(1+0.0005*iteration);
+        px = pixel(input, x, y);
+        neural.activate([x/125, y/125]);
+        neural.propagate(dynamicRate, pixel(image_data, x, y));
+      }
+    }
+    paint();
+  };
+
+  var pixel = function(data, x, y){
+    var red = data[((125 * y) + x) * 4];
+    var green = data[((125 * y) + x) * 4 + 1];
+    var blue = data[((125 * y) + x) * 4 + 2];
+    return [red/255, green/255, blue/255];
+  };
+
+  var paint = function(){
+    console.log('painting...');
+    var imageData = context.getImageData(0,0,125,125);
+    for (var x = 0; x < 125; x++){
+      for (var y = 0; y < 125; y++){
+        var rgb = neural.activate([x/125,y/125]);
+        imageData.data[((125 * y) + x) * 4] = (rgb[0] )* 255;
+        imageData.data[((125 * y) + x) * 4 + 1] = (rgb[1] ) * 255;
+        imageData.data[((125 * y) + x) * 4 + 2] = (rgb[2] ) * 255;
+      }
+    }
+    context.putImageData(imageData,0,0);
+
+    requestAnimationFrame(iterate);
+  };
+
   console.log('main controller loaded.');
 }]);
--- server/server.js
+++ server/server.js
@@ -3,8 +3,20 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var port = 3000 || process.env.PORT;
-var io = require('socket.io')(server);
+// var io = require('socket.io')(server);
 var index = require('./routes/index');
+// var paintModule = require('./modules/paint');
+
+// io.on('connection', function(socket){
+//   socket.on('error', function(err){
+//     console.log('socket error:', err);
+//   });
+//
+//   socket.emit('connection');
+//
+//   paintModule.startPainting(socket);
+//   // paintModule.sendFrame(socket);
+// });
 
 app.use(express.static('server/public'));
 
